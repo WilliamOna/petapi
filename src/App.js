6903852a100a95b36db9f8edf6e137c72120a1b8
William Ona <william.ona@revature.com>
2020-08-20 00:55:06 -0600

    added api gateway, petlambda, dynamodb for pets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
 
 Amplify.configure(config);
@@ -9,7 +9,11 @@ Amplify.configure(config);
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'> Pets!</header>
+			<header className='App-header'>
+				{' '}
+				Pets!
+				<AmplifySignOut />
+			</header>
 		</div>
 	);
 }
